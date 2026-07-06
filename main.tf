1021956253f4517bb27c3f902daeba3ba9705b0a
DevSecOps IDE Agent <sumichatt.35@gmail.com>
2026-07-06 06:45:26 +0000

    security: automated patch applied to main.tf via RNM Gatekeeper Agent

---
+++ main.tf
@@ -0,0 +1,32 @@
+resource "aws_s3_bucket" "rnm_data" {
+  bucket = "rnm-enterprise-financial-vault"
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "rnm_data_encryption" {
+  bucket = aws_s3_bucket.rnm_data.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "rnm_data_versioning" {
+  bucket = aws_s3_bucket.rnm_data.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "rnm_data_privacy" {
+  bucket                  = aws_s3_bucket.rnm_data.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
